8eb8d9981288632224f94a12ff52efecc6a2b674
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2024-07-29 21:10:23 -0400

    Added trigger for teleporting to fast travel scene from overworld

---
+++ Floating Flounders/Assets/Scripts/TouchTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TouchTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        // Debug.Log("Collision detected!");
+        LoadScene("Fast travel");
+
+    }
+
+    void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
