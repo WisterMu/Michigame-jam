d34aafc93b9e00ff5f38ca3e355e84680b0d4fc8
WisterMu <71277958+WisterMu@users.noreply.github.com>
2024-08-01 17:47:58 -0400

    Changing combat slightly. player's just a triangle instead

---
+++ Floating Flounders/Assets/Scripts/Combat Scripts/Playermovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D rb2d;
+    private Vector2 moveInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        rb2d.velocity = moveInput * moveSpeed;
+   }
+}
