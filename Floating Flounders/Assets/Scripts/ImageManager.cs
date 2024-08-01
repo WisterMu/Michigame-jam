b9835732d81f525f374d7209a48cce8bdb78325f
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2024-08-01 01:41:31 -0400

    Dialogue can now handle multiple characters displaying
    still need a "bring to front" function for showing the active speaker

--- Floating Flounders/Assets/Scripts/ImageManager.cs
+++ Floating Flounders/Assets/Scripts/ImageManager.cs
@@ -9,6 +9,7 @@ public class ImageManager : MonoBehaviour
     public Canvas canvas;
 
     public List<RawImage> characterPortraits;
+    public List<RawImage> enabledPortraits = new List<RawImage>();    // contains currently active characters
 
     // singleton stuff
     public static ImageManager Instance { get; private set; }
@@ -51,6 +52,13 @@ public class ImageManager : MonoBehaviour
     public void DisableDialogue()
     {
         canvas.enabled = false;
+
+        // clears all currently enabled portraits
+        foreach (RawImage portrait in enabledPortraits)
+        {
+            portrait.enabled = false;
+        }
+        enabledPortraits.Clear();
     }
 
     public void UpdateImage()
@@ -66,6 +74,8 @@ public class ImageManager : MonoBehaviour
             index++;
         }
         characterPortraits[index].enabled = true;
+        enabledPortraits.Add(characterPortraits[index]);
+        RearrangePortraits();
     }
 
     public void DisableCharacter(string characterName)
@@ -76,5 +86,29 @@ public class ImageManager : MonoBehaviour
             index++;
         }
         characterPortraits[index].enabled = false;
+        enabledPortraits.Remove(characterPortraits[index]);
+        RearrangePortraits();   
+    }
+    
+    // this ensures that multiple characters don't fully overlap when they appear / disappear
+    void RearrangePortraits()
+    {
+        // int offset = 0;         // offset for positioning
+        Vector3 anchor = new Vector3(7.91f, 1.90f, 95.00f);
+        float offset = 0;
+        foreach (RawImage character in enabledPortraits)
+        {
+            if (character.name == "Rovin")
+            {
+                // this is the MC
+            }
+            else
+            {
+                // offset each character portrait from each other
+                Debug.Log(character.transform.position);
+                character.transform.position = new Vector3(anchor.x + offset, anchor.y, anchor.z);
+                offset -= 1f;
+            }
+        }
     }
 }
