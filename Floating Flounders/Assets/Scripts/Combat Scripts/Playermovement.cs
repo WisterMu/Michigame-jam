c3f382bc4cd74d9f055319d2142dac6d8a239f01
WisterMu <71277958+WisterMu@users.noreply.github.com>
2024-08-02 20:27:37 -0400

    Animations in all scenes

--- Floating Flounders/Assets/Scripts/Combat Scripts/Playermovement.cs
+++ Floating Flounders/Assets/Scripts/Combat Scripts/Playermovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Playermovement : MonoBehaviour
 {
@@ -10,17 +11,19 @@ public class Playermovement : MonoBehaviour
     private Vector2 moveInput;
     public Animator animator;
 
+    private const string _lastHorizontal = "LastHorizontal";
+    private const string _lastVertical = "LastVertical";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Set(inputManager.Movement.x, inputManager.Movement.y);
 
         moveInput.Normalize();
 
@@ -29,5 +32,13 @@ public class Playermovement : MonoBehaviour
         animator.SetFloat("Horizontal", moveInput.x);
         animator.SetFloat("Vertical", moveInput.y);
         animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+
+
+        if (moveInput != Vector2.zero)
+        {
+            animator.SetFloat(_lastHorizontal, moveInput.x);
+            animator.SetFloat(_lastVertical, moveInput.y);
+        }
     }
 }
--- Floating Flounders/Assets/Scripts/Overworld Scripts/OverworldCharacterController.cs
+++ Floating Flounders/Assets/Scripts/Overworld Scripts/OverworldCharacterController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
+
 public class OverworldCharacterController : MonoBehaviour
 {
     private Rigidbody2D body;
     private Vector2 movementDirection;
     public float Speed = 5f;
     public Animator animator;
+
     private const string _lastHorizontal = "LastHorizontal";
     private const string _lastVertical = "LastVertical";
+   
 
     // public GameManager manager;
 
@@ -32,8 +35,7 @@ public class OverworldCharacterController : MonoBehaviour
         else
         {
             // Set the character's movement direction
-            movementDirection.x = Input.GetAxisRaw("Horizontal");
-            movementDirection.y = Input.GetAxisRaw("Vertical");
+            movementDirection.Set(inputManager.Movement.x, inputManager.Movement.y);
 
             movementDirection.Normalize();
         }
@@ -42,8 +44,8 @@ public class OverworldCharacterController : MonoBehaviour
         animator.SetFloat("Vertical", movementDirection.y);
         animator.SetFloat("Speed", movementDirection.sqrMagnitude);
 
-        if(movementDirection != Vector2.zero)
-        {
+        if (movementDirection != Vector2.zero)
+        { 
             animator.SetFloat(_lastHorizontal, movementDirection.x);
             animator.SetFloat(_lastVertical, movementDirection.y);
         }
