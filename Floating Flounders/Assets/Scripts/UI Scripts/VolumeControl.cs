493f56ce3cff1a9b50a9a8d6ba495fde592fa0a9
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2024-08-02 20:28:05 -0400

    Added options menu and volume slider

---
+++ Floating Flounders/Assets/Scripts/UI Scripts/VolumeControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    Slider volumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioManager.Instance.ChangeVolume(volumeSlider.value);
+    }
+}
