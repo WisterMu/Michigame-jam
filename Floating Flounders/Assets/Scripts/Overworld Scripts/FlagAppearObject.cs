b1c802088d3bc0400834b197feda6c3602b61671
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2024-08-03 14:14:36 -0400

    Implemented story 5 + 6

---
+++ Floating Flounders/Assets/Scripts/Overworld Scripts/FlagAppearObject.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlagAppearObject : MonoBehaviour
+{
+    public List<string> requiredFlags = new List<string>();
+    BoxCollider2D selfCollider = null;
+    BoxCollider2D[] parentColliders = null;
+    SpriteRenderer selfSprite = null;
+    bool hasAppeared = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selfCollider = GetComponent<BoxCollider2D>();
+        selfSprite = GetComponentInParent<SpriteRenderer>();
+        parentColliders = GetComponentsInParent<BoxCollider2D>();   // technically this also includes self collider
+        // but if the parent collider is being shown then the interact should be shown too
+
+        // disable on startup
+        selfCollider.enabled = false;
+        selfSprite.enabled = false;
+        foreach (BoxCollider2D collider in parentColliders)
+        {
+            collider.enabled = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasAppeared)
+        {
+            AttemptAppear();
+        }
+    }
+
+    private void AttemptAppear()
+    {
+        // Debug.Log(this.name + " is touching Player!");
+        bool appear = true;
+        foreach (string flag in requiredFlags)
+        {
+            if (!GameManager.Instance.GetFlag(flag))
+            {
+                // flag missing
+                appear = false;
+            }
+        }
+
+        if (appear)
+        {
+            selfCollider.enabled = true;
+            selfSprite.enabled = true;
+            foreach (BoxCollider2D collider in parentColliders)
+            {
+                collider.enabled = true;
+            }
+            hasAppeared = true;
+        }
+    }
+}
